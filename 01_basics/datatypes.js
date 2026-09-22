2c9aa1d64d8b42fb638279e6f4ef3fad38764731
Adityas1316 <adityasingh70131@gmail.com>
2026-09-22 11:04:20 +0000

    js

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,8 @@
+//varaible and its conversion
+let score ="33"
+console.log(typeof(score));
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+
+//"33"=33
+//"33abc"= NAN not a number
